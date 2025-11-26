86edaa4951e574ce50cdf118ef1e92fcc0643bce
ibrahimGoumrane <152559100+ibrahimGoumrane@users.noreply.github.com>
2025-11-26 20:13:56 +0100

    Update from Low-Code Platform

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className='bg-gray-200 dark:bg-gray-900 p-4 text-center text-gray-600 dark:text-gray-400 text-sm mt-auto'>
+      <p>&copy; All rights reserved.</p>
+    </footer>
+  );
+};
